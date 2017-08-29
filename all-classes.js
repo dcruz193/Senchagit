9dbcf5ba0d7c1b0f71d0c7796b9f6a40f86c7d2d
Daniel <canuardaniel@gmail.com>
2017-08-29 15:35:24 -0500

    archivos generados con sencha create

---
+++ all-classes.js
@@ -0,0 +1,28 @@
+/*
+Copyright(c) 2012 Company Name
+*/
+Ext.define('Daniel.grid.column.Column', {
+    extend: 'Ext.grid.column.Column',
+	alias: ['widget.dcol'],
+
+	text: 'Daniel Col',
+	dataIndex: 'dcol' 
+
+});
+Ext.define('Daniel.grid.Panel', {
+    extend: 'Ext.grid.Panel',
+	requires: ['Daniel.grid.column.Column'],
+	
+    title: 'Daniel grid',
+    columns: [
+        { xtype: 'dcol', },
+        { text: 'Name',  dataIndex: 'name' },
+        { text: 'Email', dataIndex: 'email', flex: 1 },
+        { text: 'Phone', dataIndex: 'phone' }
+    ],
+    height: 200,
+    width: 400,
+
+});
+
+
